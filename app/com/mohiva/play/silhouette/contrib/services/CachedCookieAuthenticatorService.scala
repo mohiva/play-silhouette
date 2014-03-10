1acd21b5772e3a78b320eb230210605e5b218b72
akkie <ck-github@mohiva.com>
2014-03-10 20:22:30 +0100

    Use Scala api

--- app/com/mohiva/play/silhouette/contrib/services/CachedCookieAuthenticatorService.scala
+++ app/com/mohiva/play/silhouette/contrib/services/CachedCookieAuthenticatorService.scala
@@ -22,7 +22,7 @@ package com.mohiva.play.silhouette.contrib.services
 import org.joda.time.DateTime
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
-import play.Play
+import play.api.Play
 import play.api.mvc.{ Cookie, SimpleResult, DiscardingCookie, RequestHeader }
 import com.mohiva.play.silhouette.core.{ Identity, Authenticator, LoginInfo }
 import com.mohiva.play.silhouette.core.utils.{ Clock, IDGenerator, CacheLayer }
