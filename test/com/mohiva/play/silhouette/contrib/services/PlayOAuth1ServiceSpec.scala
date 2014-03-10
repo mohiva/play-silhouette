9958b6af846e4b044568961a6c2d2859294efc8a
akkie <ck-github@mohiva.com>
2014-03-10 20:23:46 +0100

    Add missing tests

--- test/com/mohiva/play/silhouette/contrib/services/PlayOAuth1ServiceSpec.scala
+++ test/com/mohiva/play/silhouette/contrib/services/PlayOAuth1ServiceSpec.scala
@@ -13,19 +13,26 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.mohiva.play.silhouette.contrib.providers
+package com.mohiva.play.silhouette.contrib.services
 
 import org.specs2.mock.Mockito
 import org.specs2.specification.Scope
 import play.api.test.PlaySpecification
+import play.api.libs.ws.SignatureCalculator
 import play.api.libs.oauth.{ RequestToken, OAuth }
 import oauth.signpost.exception.{ OAuthException, OAuthMessageSignerException }
 import com.mohiva.play.silhouette.core.providers.{ OAuth1Info, OAuth1Settings }
 
 /**
- * Test case for the [[com.mohiva.play.silhouette.contrib.providers.PlayOAuth1Service]] class.
+ * Test case for the [[PlayOAuth1Service]] class.
  */
 class PlayOAuth1ServiceSpec extends PlaySpecification with Mockito {
+
+  "The alternative constructor" should {
+    "construct the service with the default Play Framework OAuth implementation" in new Context {
+      new PlayOAuth1Service(settings) should beAnInstanceOf[PlayOAuth1Service]
+    }
+  }
 
   "The retrieveRequestToken method" should {
     "return Failure if the token couldn't be retrieved" in new Context {
@@ -35,23 +42,23 @@ class PlayOAuth1ServiceSpec extends PlaySpecification with Mockito {
     }
 
     "return Success if the token could be retrieved" in new Context {
-      oauth.retrieveRequestToken(settings.callbackURL) returns Right(RequestToken("", ""))
+      oauth.retrieveRequestToken(settings.callbackURL) returns Right(token)
 
-      await(service.retrieveRequestToken(settings.callbackURL)) must beSuccessfulTry.withValue(OAuth1Info("", ""))
+      await(service.retrieveRequestToken(settings.callbackURL)) must beSuccessfulTry.withValue(info)
     }
   }
 
   "The retrieveAccessToken method" should {
     "return Failure if the token couldn't be retrieved" in new Context {
-      oauth.retrieveAccessToken(RequestToken("", ""), "") returns Left(new OAuthMessageSignerException(""))
+      oauth.retrieveAccessToken(token, "") returns Left(new OAuthMessageSignerException(""))
 
-      await(service.retrieveAccessToken(OAuth1Info("", ""), "")) must beFailedTry.withThrowable[OAuthException]
+      await(service.retrieveAccessToken(info, "")) must beFailedTry.withThrowable[OAuthException]
     }
 
     "return Success if the token could be retrieved" in new Context {
-      oauth.retrieveAccessToken(RequestToken("", ""), "") returns Right(RequestToken("", ""))
+      oauth.retrieveAccessToken(token, "") returns Right(token)
 
-      await(service.retrieveAccessToken(OAuth1Info("", ""), "")) must beSuccessfulTry.withValue(OAuth1Info("", ""))
+      await(service.retrieveAccessToken(info, "")) must beSuccessfulTry.withValue(info)
     }
   }
 
@@ -60,6 +67,14 @@ class PlayOAuth1ServiceSpec extends PlaySpecification with Mockito {
       oauth.redirectUrl("token") returns "http://redirect.url"
 
       service.redirectUrl("token") must be equalTo "http://redirect.url"
+    }
+  }
+
+  "The sign method" should {
+    "return the signature calculator" in new Context {
+      oauth.info returns PlayOAuth1Service.serviceInfo(settings)
+
+      service.sign(info) must beAnInstanceOf[SignatureCalculator]
     }
   }
 
@@ -78,6 +93,16 @@ class PlayOAuth1ServiceSpec extends PlaySpecification with Mockito {
       callbackURL = "https://www.mohiva.com",
       consumerKey = "my.consumer.key",
       consumerSecret = "my.consumer.secret")
+
+    /**
+     * The Silhouette OAuth1 info.
+     */
+    lazy val info = OAuth1Info("my.token", "my.secret")
+
+    /**
+     * The Play OAuth request token.
+     */
+    lazy val token = RequestToken("my.token", "my.secret")
 
     /**
      * A mock of the Play Framework OAuth implementation.
