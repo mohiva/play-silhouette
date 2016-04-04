6c01db280fec47d3c75b15329b546cfcc4d3374e
Alex Hall <alex.mojaki@gmail.com>
2016-04-04 10:51:16 +0200

    Added failing test to show that BCryptPasswordHasher needs distinct IDs

--- silhouette-password-bcrypt/build.sbt
+++ silhouette-password-bcrypt/build.sbt
@@ -2,7 +2,8 @@ import Dependencies._
 
 libraryDependencies ++= Seq(
   Library.jbcrypt,
-  Library.Specs2.core % Test
+  Library.Specs2.core % Test,
+  Library.Play.Specs2.mock % Test
 )
 
 enablePlugins(Doc)
--- silhouette-password-bcrypt/src/test/scala/com/mohiva/play/silhouette/password/BCryptPasswordHasherSpec.scala
+++ silhouette-password-bcrypt/src/test/scala/com/mohiva/play/silhouette/password/BCryptPasswordHasherSpec.scala
@@ -15,13 +15,22 @@
  */
 package com.mohiva.play.silhouette.password
 
-import com.mohiva.play.silhouette.api.util.PasswordInfo
+import com.mohiva.play.silhouette.api.LoginInfo
+import com.mohiva.play.silhouette.api.repositories.AuthInfoRepository
+import com.mohiva.play.silhouette.api.util.{Credentials, PasswordInfo}
+import com.mohiva.play.silhouette.impl.providers.CredentialsProvider
+import org.mockito.Matchers
+import org.specs2.mock.Mockito
 import org.specs2.mutable.Specification
+import play.api.libs.concurrent.Execution.Implicits._
+
+import scala.concurrent.duration.DurationInt
+import scala.concurrent.{Await, Future}
 
 /**
  * Test case for the [[BCryptPasswordHasher]] class.
  */
-class BCryptPasswordHasherSpec extends Specification {
+class BCryptPasswordHasherSpec extends Specification with Mockito {
 
   "The `hash` method" should {
     "hash a password" in {
@@ -51,6 +60,24 @@ class BCryptPasswordHasherSpec extends Specification {
       val info = hasher.hash(password)
 
       hasher.matches(info, "not-equal") must beFalse
+    }
+  }
+
+  "The `authenticate` method of CredentialsProvider" should {
+    "re-hash password with new hasher" in {
+      val credentials = Credentials("apollonia.vanova@watchmen.com", "s3cr3t")
+      val authInfoRepository = mock[AuthInfoRepository]
+      val oldHasher = new BCryptPasswordHasher(5)
+      val newHasher = new BCryptPasswordHasher(7)
+      val provider = new CredentialsProvider(authInfoRepository, newHasher, List(newHasher, oldHasher))
+      val loginInfo = LoginInfo(provider.id, credentials.identifier)
+      val passwordInfo = oldHasher.hash(credentials.password)
+
+      authInfoRepository.find[PasswordInfo](loginInfo) returns Future.successful(Some(passwordInfo))
+      authInfoRepository.update[PasswordInfo](Matchers.eq(loginInfo), Matchers.anyObject()) returns Future.successful(passwordInfo)
+
+      Await.result(provider.authenticate(credentials), 5.seconds) must be equalTo loginInfo
+      there was one(authInfoRepository).update(Matchers.eq(loginInfo), Matchers.anyObject())
     }
   }
 }
