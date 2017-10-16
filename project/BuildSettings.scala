9a54d0e35e42c48c6bae69d35e831d13c557553a
Christian Kaps <ck-github@mohiva.com>
2017-10-16 18:55:42 +0200

    Release version 5.0.2

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -31,7 +31,7 @@ object BasicSettings extends AutoPlugin {
 
   override def projectSettings = Seq(
     organization := "com.mohiva",
-    version := "5.0.1",
+    version := "5.0.2",
     resolvers ++= Dependencies.resolvers,
     scalaVersion := Dependencies.Versions.scalaVersion,
     crossScalaVersions := Dependencies.Versions.crossScala,
