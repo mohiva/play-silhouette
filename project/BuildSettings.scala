7a93ae23b923fbd2438d7a1ea3e2c1f714d96c35
Tomofumi Tanaka <tanacasino@gmail.com>
2018-04-12 11:03:24 +0900

    Update to Play 2.6.13

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -31,7 +31,7 @@ object BasicSettings extends AutoPlugin {
 
   override def projectSettings = Seq(
     organization := "com.mohiva",
-    version := "5.0.3",
+    version := "5.0.4",
     resolvers ++= Dependencies.resolvers,
     scalaVersion := Dependencies.Versions.scalaVersion,
     crossScalaVersions := Dependencies.Versions.crossScala,
